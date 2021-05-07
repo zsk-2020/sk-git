d0619ea2b027125f0c142bac71ced863450f8245
ZSK <237087188@qq.com>
2021-05-07 16:29:39 +0800

    push commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
