fe21ff909872fd1ea9ba01d1f90ab857045f8544
ZSK <237087188@qq.com>
2021-05-07 16:05:06 +0800

    second commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
